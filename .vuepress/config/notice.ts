697c0935763e57ab7b2394db2a09ebc45d839882
bujichong@163.com <bujichong@163.com>
2023-03-08 22:07:48 +0800

    *　更新配置

--- .vuepress/config/notice.ts
+++ .vuepress/config/notice.ts
@@ -3,49 +3,45 @@
  * @Email: bujichong@163.com
  * @Date: 2023-03-01 11:06:31
  * @LastEditors: bujichong
- * @LastEditTime: 2023-03-06 12:39:16
+ * @LastEditTime: 2023-03-08 22:05:37
  * @Author: bujichong
  */
 const notice =  {
-      title: '欢迎进入石火之间~~',
-      body: [
-        // {
-        //     type: "title",
-        //     content: "欢迎~~",
-        //   },
-        {
-          type: "text",
-          content: `🎉🎉🎉 爱尔前端博客已搭建完成，欢迎大家使用，如有问题， 请先看 [欢迎入驻石火之间]。`,
-          style: "font-size: 12px;",
-        },
-        // {
-        //   type: "hr",
-        // },
-        // {
-        //   type: "title",
-        //   content: "GitHub",
-        // },
-        // {
-        //   type: "text",
-        //   content: `
-        //   <ul>
-        //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-        //   </ul>`,
-        //   style: "font-size: 12px;",
-        // },
-        // {
-        //   type: "hr",
-        // },
-        // {
-        //   type: "buttongroup",
-        //   children: [
-        //     {
-        //       text: "打赏",
-        //       link: "/docs/others/donate.html",
-        //     },
-        //   ],
-        // },
-      ],
+      // title: '欢迎进入石火之间~~',
+      // body: [
+      //   {
+      //     type: "text",
+      //     content: `🎉🎉🎉 爱尔前端博客已搭建完成，欢迎大家使用，如有问题， 请先看 [欢迎入驻石火之间]。`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   // {
+      //   //   type: "hr",
+      //   // },
+      //   // {
+      //   //   type: "title",
+      //   //   content: "GitHub",
+      //   // },
+      //   // {
+      //   //   type: "text",
+      //   //   content: `
+      //   //   <ul>
+      //   //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+      //   //   </ul>`,
+      //   //   style: "font-size: 12px;",
+      //   // },
+      //   // {
+      //   //   type: "hr",
+      //   // },
+      //   // {
+      //   //   type: "buttongroup",
+      //   //   children: [
+      //   //     {
+      //   //       text: "打赏",
+      //   //       link: "/docs/others/donate.html",
+      //   //     },
+      //   //   ],
+      //   // },
+      // ],
     };
 
 export default notice;
